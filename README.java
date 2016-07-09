1aaa856b20191e700fd193c6127cc7eb5a4f3932
malathi-kpriet <malathikanakaraj14@gmail.com>
2016-07-09 16:36:13 +0530

    finish

---
+++ README.java
@@ -0,0 +1,18 @@
+# prime
+class prime
+{
+public static void main(string args[])
+{
+int num=1;
+if(num!=0)
+{
+for(i-0;i<num/2;i++)
+{
+if(num==2)
+{
+System.out.println("not a prime");
+}
+else
+{System.out.println("prime");
+}
+
